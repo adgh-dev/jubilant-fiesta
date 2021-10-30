18ee4b8033926818fc39310e8ca68624950595da
Andrei Gheorghiu <andreiebh@gmail.com>
2021-10-30 07:26:09 +0300

    initial commit

---
+++ src/main/java/ro/pakt/spring5webspp/Spring5websppApplication.java
@@ -0,0 +1,13 @@
+package ro.pakt.spring5webspp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5websppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5websppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/pakt/spring5webspp/Spring5websppApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.pakt.spring5webspp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5websppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
